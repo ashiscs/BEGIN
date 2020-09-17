dd06a2a99b97128a0c7dcb41d70fcfcad5ab0ea9
SUNANDA SAMANTA <sunandasamanta38@gmail.com>
2020-09-17 16:50:18 +0530

    Sunanda

---
+++ sunanda/practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I am sunanda</h1>
+      <a href="http://github.com/sunanda35">My github link baby</a>
+    </div>
+  );
+}
+
+export default App;
