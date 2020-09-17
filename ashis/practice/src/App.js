8412aae3c1084da62f56f8172a9ce169e856413e
ashiscs <ashissingh640@gmail.com>
2020-09-18 02:24:13 +0530

    Props
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- ashis/practice/src/App.js
+++ ashis/practice/src/App.js
@@ -8,9 +8,16 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
+      <Greet name="Dhoni" hero="Best Finisher">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Raina" hero="Best middle-order guy">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Bravo" hero="Best allrounder" />
+      <Welcome name="Dhoni" hero="Best Finisher"/>
+      <Welcome name="Raina" hero="Best Finisher"/>
+      <Welcome name="Bravo" hero="Best Finisher"/>
     </div>
   );
 }
