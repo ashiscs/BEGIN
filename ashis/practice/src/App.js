de12a32cc8016c725dde52f31ab14f13bc4b7c13
ashiscs <ashissingh640@gmail.com>
2020-09-18 02:59:36 +0530

    State
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- ashis/practice/src/App.js
+++ ashis/practice/src/App.js
@@ -4,20 +4,12 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Dhoni" hero="Best Finisher">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Raina" hero="Best middle-order guy">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Bravo" hero="Best allrounder" />
-      <Welcome name="Dhoni" hero="Best Finisher"/>
-      <Welcome name="Raina" hero="Best Finisher"/>
-      <Welcome name="Bravo" hero="Best Finisher"/>
+      <Message />
     </div>
   );
 }
