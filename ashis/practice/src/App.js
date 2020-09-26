b11e56aca3b01b074037a819760a1ce1d04eaa7f
ashiscs <ashissingh640@gmail.com>
2020-09-26 19:13:42 +0530

    Styling and CSS Basics
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- ashis/practice/src/App.js
+++ ashis/practice/src/App.js
@@ -13,11 +13,16 @@ import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import Person from "./components/Person";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
