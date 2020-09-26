6fddf53e084145b67851c966aacc5d1f99019ea7
ashiscs <ashissingh640@gmail.com>
2020-09-26 10:05:00 +0530

    List and Keys
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- ashis/practice/src/components/NameList.js
+++ ashis/practice/src/components/NameList.js
@@ -22,7 +22,9 @@ function NameList() {
       skill: "bowler",
     },
   ];
-  const personList = persons.map((person) => <Person person={person} />);
+  const personList = persons.map((person) => (
+    <Person key={person.id} person={person} />
+  ));
 
   return <div>{personList}</div>;
 }
